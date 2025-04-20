390403089755f86f592d30a62d5c356aec10b9ef
programer12345anwar <mdanwar40212@gmail.com>
2025-04-20 22:52:50 +0530

    method name corrected

--- script.js
+++ script.js
@@ -8,45 +8,45 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
-  arr.map((employee)=>{
-    if(employee.profession=="developer"){
-        console.log(employee)
+  arr.map((employee) => {
+    if (employee.profession == "developer") {
+      console.log(employee);
     }
-  })
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
-  arr.forEach((employee)=>{
-    if(employee.profession==="developer"){
-      console.log(employee)
+  arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
     }
-  })
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
-  const newEmployee={id:4,name:"susan",age:"20",profession:"intern"}
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
   arr.push(newEmployee);
   console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
-  const filteredArray=arr.filter((employee)=>{
-    return employee.profession!=="admin";
-  })
+  const filteredArray = arr.filter((employee) => {
+    return employee.profession !== "admin";
+  });
   console.log(filteredArray);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
-  const newArray=[
-    {id:5,name:"anwar",age:24,profession:"developer"},
-    {id:6,name:"ahmad",age:25,profession:"intern"},
-    {id:7,name:"ali",age:26,profession:"intern"},
-    {id:8,name:"Naved",age:27,profession:"admin"}
-  ]
-  const concatenatedArray=arr.concat(newArray);
+  const newArray = [
+    { id: 5, name: "anwar", age: 24, profession: "developer" },
+    { id: 6, name: "ahmad", age: 25, profession: "intern" },
+    { id: 7, name: "ali", age: 26, profession: "intern" },
+    { id: 8, name: "Naved", age: 27, profession: "admin" },
+  ];
+  const concatenatedArray = arr.concat(newArray);
   console.log(concatenatedArray);
 }
